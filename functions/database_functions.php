9ef23ef98d0b44d82cd8c5b088e4224c12dccb72
huy <doanvanhuy0907@gmail.com>
2020-05-21 12:45:42 +0700

    make it better

--- functions/database_functions.php
+++ functions/database_functions.php
@@ -44,8 +44,8 @@
 	}
 
 	function insertIntoOrder($conn, $customerid, $total_price, $date, $ship_name, $ship_address, $ship_city, $ship_zip_code, $ship_country){
-		$query = "INSERT INTO orders VALUES 
-		('', ' . $customerid . ', '" . $total_price . "', '" . $date . "', '" . $ship_name . "', '" . $ship_address . "', '" . $ship_city . "', '" . $ship_zip_code . "', '" . $ship_country . "')";
+		$query = INSERT INTO orders VALUES 
+		('', '" . $customerid . "', '" . $total_price . "', '" . $date . "', '" . $ship_name . "', '" . $ship_address . "', '" . $ship_city . "', '" . $ship_zip_code . "', '" . $ship_country . "');
 		$result = mysqli_query($conn, $query);
 		if(!$result){
 			echo "Insert orders failed " . mysqli_error($conn);
